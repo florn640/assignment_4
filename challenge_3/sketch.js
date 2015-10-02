f1e88538d20edeaaa69952f5c81b08cd60832d04
nat1508 <florn640@newschool.edu>
2015-10-02 16:52:05 -0400

    challenge 3

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -20,6 +20,11 @@ function draw() {
 
 	stroke(194, 41, 53);
 	strokeWeight(18);
+	if (mouseX > 200)
+		mouseX = 200;
+
+
+
 	line(mouseX + 40, mouseY + 30, mouseX + 85, mouseY + 80);
 	//	line(40, 30, 85, 80);
 	line(mouseX + 85, mouseY + 30, mouseX + 40, mouseY + 80);
